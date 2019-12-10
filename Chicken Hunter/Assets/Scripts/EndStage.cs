26ed5519fc47bbc722148f5f68af8f0d1eb4a2f5
Tri Edy <49435622+EdyW98@users.noreply.github.com>
2019-12-11 02:02:08 +0700

    Set Next Stage
    next stage done. kurang atur collider di map

--- Chicken Hunter/Assets/Scripts/EndStage.cs
+++ Chicken Hunter/Assets/Scripts/EndStage.cs
@@ -5,26 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class EndStage : MonoBehaviour
 {
-    public int nextLevel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //  public int nextLevel;
+    //  // Start is called before the first frame update
+    //  void Start()
+    //  {
 
-    // Update is called once per frame
-    void Update()
+    //  }
+
+    //  // Update is called once per frame
+    //  void Update()
+    //  {
+
+    //  }
+
+    //  void OnTriggerEnter2D(Collider2D other)
+    //  {
+    //if(other.CompareTag("Player"))
+    //{
+    //          //Debug.Log("You Win");
+    //          //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+    //          Application.LoadLevel(nextLevel);
+    //}
+    //  }
+
+    [SerializeField] private string nextStage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-    
-    void OnTriggerEnter2D(Collider2D other)
-    {
-		if(other.CompareTag("Player"))
-		{
-            //Debug.Log("You Win");
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
-            Application.LoadLevel(nextLevel);
-		}
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextStage);
+        }
     }
 }
