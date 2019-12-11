ad898ec7bf780ba4006ab41c806ebce059f13d83
Tri Edy <49435622+EdyW98@users.noreply.github.com>
2019-12-11 11:00:42 +0700

    Revert "Set Next Stage"
    This reverts commit 26ed5519fc47bbc722148f5f68af8f0d1eb4a2f5.

--- Chicken Hunter/Assets/Scripts/EndStage.cs
+++ Chicken Hunter/Assets/Scripts/EndStage.cs
@@ -5,36 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class EndStage : MonoBehaviour
 {
-    //  public int nextLevel;
-    //  // Start is called before the first frame update
-    //  void Start()
-    //  {
-
-    //  }
-
-    //  // Update is called once per frame
-    //  void Update()
-    //  {
-
-    //  }
-
-    //  void OnTriggerEnter2D(Collider2D other)
-    //  {
-    //if(other.CompareTag("Player"))
-    //{
-    //          //Debug.Log("You Win");
-    //          //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
-    //          Application.LoadLevel(nextLevel);
-    //}
-    //  }
-
-    [SerializeField] private string nextStage;
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    public int nextLevel;
+    // Start is called before the first frame update
+    void Start()
     {
-        if (collision.CompareTag("Player"))
-        {
-            SceneManager.LoadScene(nextStage);
-        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+		if(other.CompareTag("Player"))
+		{
+            //Debug.Log("You Win");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+            Application.LoadLevel(nextLevel);
+		}
     }
 }
